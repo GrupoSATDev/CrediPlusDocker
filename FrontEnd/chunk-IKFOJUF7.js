be42d6a91d4e9b00eb38a00ad3cd380c97018ca0
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-09-19 09:16:34 -0500

    Avances Front

--- FrontEnd/chunk-IKFOJUF7.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var t=[{path:"",children:[{path:"solicitudes",loadChildren:()=>import("./chunk-MV3GEC7F.js")},{path:"tasas",loadChildren:()=>import("./chunk-NEPN5SFL.js")},{path:"tipos-pagos",loadChildren:()=>import("./chunk-EMSIP4GN.js")},{path:"creditos",loadChildren:()=>import("./chunk-LFILGK3T.js")}]}];export{t as default};
---
+++ FrontEnd/chunk-VXZNN57S.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empleados",loadChildren:()=>import("./chunk-T2JQMMNL.js")}]}];export{e as default};
