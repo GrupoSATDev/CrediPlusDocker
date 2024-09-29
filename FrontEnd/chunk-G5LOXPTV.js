f893098612fed2471cea64315972a6f4f2cb3fa5
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-09-29 08:59:29 -0500

    Ajustes - Front Botones

---
+++ FrontEnd/chunk-G5LOXPTV.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var o=[{path:"",children:[{path:"cobros",loadChildren:()=>import("./chunk-JKKKR6NV.js")}]}];export{o as default};
--- FrontEnd/chunk-URCBJDYR.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empleados",loadChildren:()=>import("./chunk-E4JGQMAJ.js")}]}];export{e as default};
