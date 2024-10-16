897db8b0090df4b3e1855c9a3e4b2e68eef48081
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-10-15 20:36:38 -0500

    Ajustes Responsive

--- FrontEnd/chunk-EGB7FUNA.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var t=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-T5TUNYU2.js")}]}];export{t as default};
---
+++ FrontEnd/chunk-NINAMPPS.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var o=[{path:"",children:[{path:"cobros",loadChildren:()=>import("./chunk-L6S7ZKW2.js")}]}];export{o as default};
