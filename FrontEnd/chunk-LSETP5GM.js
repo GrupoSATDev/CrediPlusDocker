634215958dfd3d04f4f912f7d303b36cda70d0d3
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-10-17 22:11:38 -0500

    Front

---
+++ FrontEnd/chunk-LSETP5GM.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var o=[{path:"",children:[{path:"cobros",loadChildren:()=>import("./chunk-O57YCCJA.js")}]}];export{o as default};
--- FrontEnd/chunk-NINAMPPS.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var o=[{path:"",children:[{path:"cobros",loadChildren:()=>import("./chunk-L6S7ZKW2.js")}]}];export{o as default};
