5dc84e1cfa9a1a0e61d6a118f47121d18b20f51f
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-10-24 22:26:18 -0500

    Ajustes

--- Usuarios/chunk-4ODT4FLD.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var t=[{path:"",children:[{path:"solicitudes",loadChildren:()=>import("./chunk-LTVTV34Z.js")},{path:"creditos",loadChildren:()=>import("./chunk-YHKC4HLL.js")}]}];export{t as default};
--- Usuarios/chunk-D6U4B5S2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var o=[{path:"",children:[{path:"cobros",loadChildren:()=>import("./chunk-NZXXDBCL.js")}]}];export{o as default};
