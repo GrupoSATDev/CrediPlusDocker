457347e0c4578b81a5c750dc89223091a74e3e25
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-11-04 22:44:00 -0500

    Front Admin - Product

---
+++ FrontEnd/chunk-AGVASLKU.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"trabajadores",loadChildren:()=>import("./chunk-C7KAQFZI.js")}]}];export{a as default};
---
+++ FrontEnd/chunk-QXR3SYZ4.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-O3P7L6I6.js")},{path:"bancos",loadChildren:()=>import("./chunk-6ORI4VV5.js")}]}];export{a as default};
