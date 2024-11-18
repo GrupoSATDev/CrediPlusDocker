5e6a6fa06f990193b270ba5402158f2c37e67950
jdcsalej <51374452+jdcsalej@users.noreply.github.com>
2024-11-17 23:33:31 -0500

    Ajuste Front

--- FrontEnd/chunk-DN7SY3TX.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-MDHKUFZT.js")},{path:"bancos",loadChildren:()=>import("./chunk-BNBY7CQF.js")}]}];export{a as default};
