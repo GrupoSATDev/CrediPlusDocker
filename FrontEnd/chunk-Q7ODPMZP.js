37f9652ba62efaeca060d75530d5c7dfe8eb36c5
jdcsalej <51374452+jdcsalej@users.noreply.github.com>
2024-11-18 14:40:24 -0500

    Rollback

---
+++ FrontEnd/chunk-Q7ODPMZP.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-DM2QJFSW.js")},{path:"bancos",loadChildren:()=>import("./chunk-STZLZIBI.js")}]}];export{a as default};
