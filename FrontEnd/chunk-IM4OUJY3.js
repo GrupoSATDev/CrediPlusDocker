906c787ecda4d38c6a133cf5100eb29fcda4632c
jdcsalej <51374452+jdcsalej@users.noreply.github.com>
2024-11-20 16:25:30 -0500

    Prueba de PWA

--- FrontEnd/chunk-IM4OUJY3.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-YNKPP2WM.js")},{path:"bancos",loadChildren:()=>import("./chunk-FJB4WPBI.js")}]}];export{a as default};
---
+++ FrontEnd/chunk-ZS2UVYZW.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empresas",loadChildren:()=>import("./chunk-3HM4HZGW.js")}]}];export{e as default};
