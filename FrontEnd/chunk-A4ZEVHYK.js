77087979c31e81c014d8fdd039ada4fc7d1b2980
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-11-20 08:27:48 -0500

    Avances

---
+++ FrontEnd/chunk-A4ZEVHYK.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"trabajadores",loadChildren:()=>import("./chunk-AXMIHBDH.js")}]}];export{a as default};
---
+++ FrontEnd/chunk-VC5EKXWK.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empresas",loadChildren:()=>import("./chunk-IBMWY3OB.js")}]}];export{e as default};
