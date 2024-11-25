8113605b85dad1bf539af58b97e1146b816b5c47
jdcsalej <51374452+jdcsalej@users.noreply.github.com>
2024-11-24 19:27:13 -0500

    URL de conexión

---
+++ Usuarios/chunk-MORQQ7WM.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var t=[{path:"",children:[{path:"solicitudes",loadChildren:()=>import("./chunk-O54GMKOQ.js")},{path:"creditos",loadChildren:()=>import("./chunk-YISJLYZY.js")}]}];export{t as default};
