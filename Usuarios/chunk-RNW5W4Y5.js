581d296506c4d08fdc6e02bc46e5ab487c081a77
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-11-25 10:49:37 -0500

    Ajuste PWA

---
+++ Usuarios/chunk-RNW5W4Y5.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var t=[{path:"",children:[{path:"solicitudes",loadChildren:()=>import("./chunk-GF2NIQV5.js")},{path:"creditos",loadChildren:()=>import("./chunk-GUN5ZTSB.js")}]}];export{t as default};
