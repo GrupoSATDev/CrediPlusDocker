f335ae4097bb989d753cbd029ad5e09fde076667
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-11-25 19:56:39 -0500

    Ajustes Front

---
+++ FrontEnd/chunk-LVUUGYN2.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var t=[{path:"",children:[{path:"solicitudes",loadChildren:()=>import("./chunk-YTGLY4TF.js")},{path:"creditos",loadChildren:()=>import("./chunk-NHD3FBY7.js")}]}];export{t as default};
--- Usuarios/chunk-2NI3X6DJ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var o=[{path:"",children:[{path:"cobros",loadChildren:()=>import("./chunk-LFXBSXAX.js")}]}];export{o as default};
