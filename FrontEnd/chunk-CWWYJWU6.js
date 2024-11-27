89abb6283466bd363b29272359f8fbd208774458
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-11-27 08:25:37 -0500

    Ajustes Frotn

--- FrontEnd/chunk-CWWYJWU6.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empresas",loadChildren:()=>import("./chunk-G4NMFIC3.js")}]}];export{e as default};
--- FrontEnd/chunk-ZKOUSDRY.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-26NOGWHU.js")},{path:"bancos",loadChildren:()=>import("./chunk-3B7GPW5T.js")}]}];export{a as default};
