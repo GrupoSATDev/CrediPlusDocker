b24215fdbfd79dd3a0afa727475b5bd669c243e1
jdcsalej <51374452+jdcsalej@users.noreply.github.com>
2024-12-01 12:04:31 -0500

    Ajustes Front

---
+++ FrontEnd/chunk-YQNYRWQM.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empresas",loadChildren:()=>import("./chunk-ZPCWVNJK.js")}]}];export{e as default};
---
+++ FrontEnd/chunk-ZGEOAGPS.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"trabajadores",loadChildren:()=>import("./chunk-OHWKDEPP.js")}]}];export{a as default};
