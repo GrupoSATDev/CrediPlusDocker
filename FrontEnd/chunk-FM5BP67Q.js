ac54bb864fa789bd07894470557ee2e2ceb91512
jdcsalej <51374452+jdcsalej@users.noreply.github.com>
2025-01-05 13:57:51 -0500

    Ajustes Varios

---
+++ FrontEnd/chunk-FM5BP67Q.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empresas",loadChildren:()=>import("./chunk-BLENAYRD.js")}]}];export{e as default};
--- FrontEnd/chunk-LEMANWKJ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var e=[{path:"",children:[{path:"empresas",loadChildren:()=>import("./chunk-7Y274SPE.js")}]}];export{e as default};
