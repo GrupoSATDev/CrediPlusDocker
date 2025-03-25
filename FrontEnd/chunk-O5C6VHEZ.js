fdd95dea98acf4a16b7ad90581bbcd5b790670ad
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2025-03-24 22:08:56 -0500

    Reportes  CrediPlus

---
+++ FrontEnd/chunk-O5C6VHEZ.js
@@ -0,0 +1,1 @@
+import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"cuentas",loadChildren:()=>import("./chunk-3IUQXECB.js")},{path:"bancos",loadChildren:()=>import("./chunk-KRA4YU64.js")}]}];export{a as default};
--- FrontEnd/chunk-PUIQ257T.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-NEB6MB4Y.js";var a=[{path:"",children:[{path:"trabajadores",loadChildren:()=>import("./chunk-MM5T2NSM.js")}]}];export{a as default};
